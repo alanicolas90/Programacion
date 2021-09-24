3cca61aa22d62d25917e3e1cddb6568b5e343e6e
alani <alanicolas90@gmail.com>
2021-09-24 09:11:00 +0200

    ejercicios finalizados

--- alternativas/src/main/java/main/Alternativas15.java
+++ alternativas/src/main/java/main/Alternativas15.java
@@ -9,29 +9,32 @@ public class Alternativas15 {
         System.out.print("Dime la cantidad de alumnos que van de viaje de estudios: ");
         int cantidadAlumnos=sc.nextInt();
 
+        double pagoAutobusPorAlumno=(4000/30)*cantidadAlumnos;
+
         if (cantidadAlumnos>=100){
             System.out.println("A cada alumno le tiene que cobrar 65€");
-            System.out.println("A la compañia le tiene que pagar 4000€.");
+            System.out.println("A la compañia le tiene que pagar"+pagoAutobusPorAlumno+"€.");
            //65 por alumno
         }
         else if(cantidadAlumnos<100 && cantidadAlumnos>=50){
             System.out.println("A cada alumno le tiene que cobrar 70€.");
-            System.out.println("A la compañia le tiene que pagar 4000€.");
+            System.out.println("A la compañia le tiene que pagar"+pagoAutobusPorAlumno+"€.");
             //70 por alumno
         }
         else if(cantidadAlumnos<50 && cantidadAlumnos>=30){
             System.out.println("A cada alumno le tiene que cobrar 95€.");
-            System.out.println("A la compañia le tiene que pagar 4000€.");
+            System.out.println("A la compañia le tiene que pagar"+pagoAutobusPorAlumno+"€.");
             //95 por alumno
         }
         else if(cantidadAlumnos<30  && cantidadAlumnos>=1){
-            System.out.println("A cada alumno le tiene que cobrar"+"€.");
+            int pagoPorAlumno=4000/cantidadAlumnos;
+            System.out.println("A cada alumno le tiene que cobrar"+pagoPorAlumno+"€.");
             System.out.println("A la compañia le tiene que pagar 4000€.");
             //x
         }
         else{
             System.out.println("No existen alumnos negativos pardillo.");
-            }
+        }
 
 
     }
--- alternativas/src/main/java/main/Principal.java
+++ alternativas/src/main/java/main/Principal.java
@@ -422,7 +422,35 @@ public class Principal {
                 break;
             }
             else if (numeroEjercicio==15){
+                System.out.print("Dime la cantidad de alumnos que van de viaje de estudios: ");
+                int cantidadAlumnos=sc.nextInt();
 
+                double pagoAutobusPorAlumno=(4000/30)*cantidadAlumnos;
+
+                if (cantidadAlumnos>=100){
+                    System.out.println("A cada alumno le tiene que cobrar 65€");
+                    System.out.println("A la compañia le tiene que pagar"+pagoAutobusPorAlumno+"€.");
+                    //65 por alumno
+                }
+                else if(cantidadAlumnos<100 && cantidadAlumnos>=50){
+                    System.out.println("A cada alumno le tiene que cobrar 70€.");
+                    System.out.println("A la compañia le tiene que pagar"+pagoAutobusPorAlumno+"€.");
+                    //70 por alumno
+                }
+                else if(cantidadAlumnos<50 && cantidadAlumnos>=30){
+                    System.out.println("A cada alumno le tiene que cobrar 95€.");
+                    System.out.println("A la compañia le tiene que pagar"+pagoAutobusPorAlumno+"€.");
+                    //95 por alumno
+                }
+                else if(cantidadAlumnos<30  && cantidadAlumnos>=1){
+                    int pagoPorAlumno=4000/cantidadAlumnos;
+                    System.out.println("A cada alumno le tiene que cobrar"+pagoPorAlumno+"€.");
+                    System.out.println("A la compañia le tiene que pagar 4000€.");
+                    //x
+                }
+                else{
+                    System.out.println("No existen alumnos negativos pardillo.");
+                }
                 break;
             }
             else if (numeroEjercicio==16){
