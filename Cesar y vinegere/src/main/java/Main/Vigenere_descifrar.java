e9a05c98f0a8ce6941952429ddaa9f12e3132c72
Alan-Nicolas Mikolajczyk <alanicolas90@gmail.com>
2021-10-21 12:58:18 +0200

    probando el branch de develop

--- Cesar y vinegere/src/main/java/Main/Vigenere_descifrar.java
+++ Cesar y vinegere/src/main/java/Main/Vigenere_descifrar.java
@@ -22,6 +22,7 @@ public class Vigenere_descifrar {
                 posClave = (i+contador) % cifrado.length();
                 letraClave = cifrado.charAt(posClave);
                 cambio = letraClave - 97;
+
                 letraNueva = fraseCodificar.charAt(i);
                 int sumaChares = letraNueva - cambio - 97;
                 letraNueva = (char) (((sumaChares+26) % 26) + 97);
@@ -31,6 +32,7 @@ public class Vigenere_descifrar {
                 posClave = (i+contador) % cifrado.length();
                 letraClave = cifrado.charAt(posClave);
                 cambio = letraClave - 65;
+
                 letraNueva = fraseCodificar.charAt(i);
                 int sumaChares = letraNueva - cambio - 65;
                 letraNueva = (char) (((sumaChares +26) % 26) + 65);
