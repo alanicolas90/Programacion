d4a8ef9f363ee4a7f0922ba54ca7abaf3c272452
Alan-Nicolas Mikolajczyk <alanicolas90@gmail.com>
2021-10-26 23:45:44 +0200

    faltan ultimos retoques y arreglado por ahora

---
+++ Ahorcado/src/main/java/main/Main.java
@@ -0,0 +1,99 @@
+package main;
+
+import com.github.javafaker.Faker;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        StringBuilder palabraOcultada = new StringBuilder();
+        StringBuilder letrasUtilizadas = new StringBuilder();
+
+        // definir valores iniciales
+        int intentosRestantes = 9;
+        char letraPrueba = 0;
+        boolean adivinado = false;
+        String intentoAdivinar;
+
+        //palabra a encontrar
+        Faker crearPalabra = new Faker();
+        String palabra = crearPalabra.animal().name();//palabra que queremos encontrar(serán animales en inglés)
+
+        // la palabra ocultada.
+        for (int letraPalabra = 0; letraPalabra < palabra.length(); letraPalabra++) {
+            if (Character.isLetter(palabra.charAt(letraPalabra))) {
+                palabraOcultada.append("*");
+            } else {
+                palabraOcultada.append(palabra.charAt(letraPalabra));
+            }
+        }
+        System.out.println(palabraOcultada);
+        // el numero de oportunidades
+        do {
+            do {
+                System.out.println("Tienes " + intentosRestantes + " intentos restantes.");
+                System.out.println("Las letras utilizadas y no estan en la palabra:" + letrasUtilizadas);
+                System.out.println("Escribeme la siguiente letra(escriba solo un caracter): ");
+                intentoAdivinar = sc.nextLine().toLowerCase();
+                //que sea una sola letra
+
+                if (intentoAdivinar.length() == palabra.length()) {
+                    if (intentoAdivinar.equals(palabra)) {
+                        adivinado = true;
+                    } else {
+                        intentosRestantes--;
+                        Dibujitos.extracted(intentosRestantes);
+                    }
+                } else if (intentoAdivinar.length() != 1) {
+                    System.out.println("VALOR INCORRECTO, porfavor introduzca solo 1 caracter porfavor o la palabra entera(que tenga la longitud de la palabra o será invalido:");
+                    intentoAdivinar = sc.nextLine().toLowerCase();
+                }
+                // pedirle letra, comprobar que la letra sea nueva
+                //saber si la letra ha sido utilizado antes o no
+                if (letrasUtilizadas.indexOf(intentoAdivinar)!=-1) {
+                    System.out.println("La letra escrita ya la has utilizado, porfavor pruebe con otro: ");
+                    intentoAdivinar = sc.nextLine().toLowerCase();
+                }
+            } while (!adivinado && intentoAdivinar.length() != 1 || letrasUtilizadas.indexOf(intentoAdivinar) != -1);
+            letrasUtilizadas.append(intentoAdivinar);
+            //si la letra está o no en la palabra
+            if (palabra.contains(intentoAdivinar)) {
+                // DIFICIL mostrar palabra ocultada *** ****   *A* *****
+                if (intentoAdivinar.length() == 1) {
+                    letraPrueba=intentoAdivinar.charAt(0);
+                    for (int i = 0; i < palabra.length(); i++) {
+
+                        if (palabra.charAt(i) == letraPrueba) {
+                            palabraOcultada.setCharAt(i, palabra.charAt(i));
+                        }
+                    }
+                    System.out.println(palabraOcultada);
+                }
+            }
+            //si la letra no está que me quite un intento
+            else {
+                System.out.println("la letra no está en la palabra");
+                intentosRestantes--;
+                System.out.println(palabraOcultada);
+            }
+
+            //bucle hasta oportunidades o que acierte
+
+            if (palabraOcultada.toString().equals(palabra)) {
+                adivinado = true;
+            }
+            // si no esta saco ahorcado
+            Dibujitos.extracted(intentosRestantes);
+
+        } while (!adivinado && intentosRestantes > 0);
+        if (adivinado) {
+            System.out.println("FELICIDADES HAS GANADO");
+        } else {
+            System.out.println("HAS PERDIDO");
+            System.out.println("La solución era: " + palabra);
+        }
+        System.out.println("Se acabó la partida.");
+    }
+
+}
