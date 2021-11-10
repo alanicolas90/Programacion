29dc122ff149647ce9f0de037e5a258e88b3464b
Alan-Nicolas Mikolajczyk <alanicolas90@gmail.com>
2021-11-10 12:33:36 +0100

    cositassssss

--- ej17Arrays/src/main/java/main/Ejercicio17.java
+++ ej17Arrays/src/main/java/main/Ejercicio17.java
@@ -8,7 +8,25 @@ public class Ejercicio17 {
         int[] cartas = new int[20];
         int[] jugador1 = new int[10];
         int[] jugador2 = new int[10];
+        int position = 0;
         servicios.crearBarajaEj17(cartas);
+        servicios.shuffleBaraja(cartas);
+
+        //repartimos las barajas entre los jugadores
+        for (int i = 0; i < cartas.length; i++, position++) {
+            jugador1[position] = cartas[i];
+            i++;
+            jugador2[position] = cartas[i];
+        }
+        System.out.println("JUGADOR 1");
+        for (int i = 0; i < jugador1.length; i++) {
+            System.out.print(jugador1[i] + " ");
+        }
+        System.out.println();
+        System.out.println("JUGADOR 2");
+        for (int i = 0; i < jugador2.length; i++) {
+            System.out.print(jugador2[i] + " ");
+        }
 
 
     }
--- ej17Arrays/src/main/java/servicios/ServiciosCartas.java
+++ ej17Arrays/src/main/java/servicios/ServiciosCartas.java
@@ -1,23 +1,36 @@
 package servicios;
 
+import java.util.Random;
+
 public class ServiciosCartas {
     public void crearBarajaEj17(int[] cartas) {
         for (int i = 0; i < 20; i++) {
-            cartas[i] = (i + 1) % 10;
+            cartas[i] = (i % 10) + 1;
         }
     }
 
     public void crearBarajaSpain(int[] cartas) {
         for (int i = 0; i < 40; i++) {
-            cartas[i] = (i + 1) % 10;
+            cartas[i] = (i % 10) + 1;
         }
     }
 
     public void crearBarajaEnglish(int[] cartas) {
         for (int i = 0; i < 52; i++) {
-            cartas[i] = (i + 1) % 12;
+            cartas[i] = (i % 12) + 1;
         }
     }
 
+    public void shuffleBaraja(int[] cartas) {
+        Random r = new Random();
+        for (int i = 0; i < 1000; i++) {
+            int randomPosition1 = r.nextInt(cartas.length);
+            int randomPosition2 = r.nextInt(cartas.length);
+            int saveCard = cartas[randomPosition1];
 
+            cartas[randomPosition1] = cartas[randomPosition2];
+            cartas[randomPosition2] = saveCard;
+
+        }
+    }
 }
