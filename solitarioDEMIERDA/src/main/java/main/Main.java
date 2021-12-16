7f4afcc0a583da91491c7fef3960848f62632515
Alan-Nicolas Mikolajczyk <alanicolas90@gmail.com>
2021-12-16 09:03:03 +0100

    trrabajo tocho web y comienzo tres en raya

--- solitarioDEMIERDA/src/main/java/main/Main.java
+++ solitarioDEMIERDA/src/main/java/main/Main.java
@@ -1,8 +1,25 @@
 package main;
 
+import dao.Dao;
+
+import java.util.Arrays;
+
 public class Main {
 
     public static void main(String[] args) {
+        Dao dao = new Dao();
+
+        dao.generarBaraja();
+        System.out.println(Arrays.toString(dao.imprimirBaraja()));
+        dao.shuffle();
+        System.out.println(Arrays.toString(dao.imprimirBaraja()));
+
+        int counter = 0;
+        do {
+            System.out.println(dao.sacarCarta());
+            System.out.println(dao.sacarCarta());
+            counter++;
+        } while (counter != 20);
 
 
     }
