6b2548112e8e8977d0f3be6d88426b6376006238
alanicolas90 <91051723+alanicolas90@users.noreply.github.com>
2022-04-25 10:06:23 +0200

    carpetas colocadas y 2 ejer marcas metido

---
+++ MARKUP/practica2Marcar3erTri/practica2.js
@@ -0,0 +1,69 @@
+const CODIGO=25;
+
+function misterio1(document,id){
+    if(document.getElementById(id)){
+        return document.getElementById(id).innerHTML;
+    }
+    else{
+        return "No existe ese id";
+    }
+}
+
+function misterio2(){
+    return true;
+}
+function misterio3(){
+    return true;
+}
+function misterio4(){
+    return true;
+}
+function misterio5(){
+    return true;
+}
+function misterio6(){
+    return true;
+}
+function misterio7(){
+    return true;
+}
+function misterio8(){
+    return true;
+}
+function misterio9(){
+    return true;
+}
+function misterio10(){
+    return true;
+}
+function misterio11(){
+    return true;
+}
+function misterio12(){
+    return true;
+}
+function misterio13(){
+    return true;
+}
+function misterio14(){
+    return true;
+}
+
+
+ module.exports = {
+    misterio1,
+    misterio2,
+    misterio3,
+    misterio4,
+    misterio5,
+    misterio6,
+    misterio7,
+    misterio8,
+    misterio9,
+    misterio10,
+    misterio11,
+    misterio12,
+    misterio13,
+    misterio14,
+    CODIGO
+}
